987de68e0f2de5bc4214bdbd2d8ed6893e217916
Saeed Chabok <85587352+saeedchabok@users.noreply.github.com>
2023-01-22 22:30:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <HeaderApp/>
+</template>
+
+<script>
+import HeaderApp from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+        <p>{{ nr }}</p>
+        <p v-if='gender'>man</p>
+        <p v-else>woman</p>
+        <p v-for="customer in customers" :key="customer.id">
+ {{ customer.name }}, {{ customer.city }}</p>
+    </div>
+
+
+
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components:
+        {},
+    data: () => ({
+        name: 'Saeed',
+        nr: 1,
+        gender: true,
+        customers:
+            [
+                { id: 1, name: 'ali', city: 'berlin' },
+                { id: 2, name: 'max', city: 'bremen' }
+            ]
+    })
+}
+</script>
+
+<style  scoped>
+
+</style>
